14ac5ad580400d8087fba521993d36be10f06a3c
Yulka <yupostupinskaya@sdvor.com>
2021-10-25 00:50:51 +0500

    add animation on main screen

--- lib/widget/main_chapters.dart
+++ lib/widget/main_chapters.dart
@@ -13,51 +13,62 @@ class MainChapters extends StatefulWidget {
   State<MainChapters> createState() => _MainChaptersState();
 }
 
-class _MainChaptersState extends State<MainChapters> with TickerProviderStateMixin {
+class _MainChaptersState extends State<MainChapters> with SingleTickerProviderStateMixin {
+
+  static const Color beginColor = Colors.deepPurple;
+  static const Color endColor = Colors.deepOrange;
+  Duration duration = const Duration(milliseconds: 500);
+  late AnimationController controller;
+  late Animation<double> animation;
+
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = AnimationController(vsync: this, duration: duration, lowerBound: 0.6, upperBound: 1.0);
+    animation =
+        CurvedAnimation(parent: controller, curve: Curves.easeInOutBack);
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+
   @override
   Widget build(BuildContext context) {
-
-    late final AnimationController _controller = AnimationController(
-      duration: const Duration(seconds: 10),
-      vsync: this,
-    )..repeat();
-
-
-
-    @override
-    void dispose() {
-      _controller.dispose();
-      super.dispose();
-    }
 
     return Flexible(
       child: InkWell(
         onTap: widget.press,
+        focusColor: Colors.transparent,
+        hoverColor: Colors.transparent,
         child: Column(
+          mainAxisSize: MainAxisSize.min,
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             Text(widget.text, style: TextStyle(fontSize: 32, color: Colors.black),),
             MouseRegion(
               onEnter: (event){
                 print(event);
+                controller.forward();
               },
-              child: AnimatedBuilder(
-                animation: _controller,
+              onExit: (event){
+                controller.reverse();
+              },
+              child: ScaleTransition(
+                scale: animation,
                 child: Container(
-                  width: 200.0,
-                  height: 200.0,
-                  color: Colors.green,
+                  padding: EdgeInsets.symmetric(vertical: 50),
                   child: CircleAvatar(
+                    backgroundColor: Colors.transparent,
                     backgroundImage: AssetImage(widget.image),
-                    radius: MediaQuery.of(context).size.height/4,
+                    radius: MediaQuery.of(context).size.height/4  ,
                   ),
                 ),
-                builder: (BuildContext context, Widget? child) {
-                  return Transform.scale(
-                    scale: 1.3,
-                    child: child,
-                  );
-                },
               ),
 
             ),
